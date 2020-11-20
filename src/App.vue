1b9489609e1b6e31a5b73b2e1ace087ee4f28ae5
Rasmus Sørensen <xxr@live.dk>
2020-11-20 08:52:49 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <div class="input" v-for="input in inputs" :key="input.id">
+    <div
+      class="input"
+      v-for="input in inputs"
+      :key="input.id"
+    >
       <div>{{ input.title }}</div>
       <vue-slider
         v-model="input.value"
@@ -18,18 +22,28 @@
 </template>
 
 <script>
-import 'https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js';
+import mqtt from 'mqtt';
 import VueSlider from 'vue-slider-component';
 import 'vue-slider-component/theme/antd.css';
-import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     VueSlider
   },
-  data () {
+  data() {
     return {
+      mqtt: {
+        client: null,
+        host: '10.142.106.136',
+        port: 9002,
+        path: '/ws',
+        clean: true,
+        protocol: 'ws',
+        username: 'robotarm',
+        password: 'robotarm',
+        clientId: 'web_' + parseInt(Math.random() * 100, 10),
+      },
       inputs: [
         {
           id: 'S1',
@@ -46,11 +60,11 @@ export default {
           id: 'S2',
           type: 'SERVO',
           title: 'Servo 2 (Shoulder)',
-          value: 80,
+          value: 70,
           adsorb: true,
           interval: 5,
           min: 0,
-          max: 180,
+          max: 150,
           marks: true,
         },
         {
@@ -61,7 +75,7 @@ export default {
           adsorb: true,
           interval: 5,
           min: 0,
-          max: 180,
+          max: 150,
           marks: true,
         },
         {
@@ -94,45 +108,101 @@ export default {
           adsorb: true,
           interval: 5,
           min: 0,
-          max: 180,
+          max: 80,
           marks: true,
         },
         {
           id: 'SPEED',
           type: 'SPEED',
           title: 'Servo speed',
-          value: 20,
+          value: 8,
           adsorb: true,
           interval: 1,
-          min: 5,
+          min: 3,
           max: 50,
           marks: false,
         },
       ],
     };
   },
+  mounted() {
+    this.connectMQTT();
+
+    this.resetAll();
+  },
   methods: {
+    connectMQTT() {
+      this.mqtt.client = mqtt.connect(
+        `${this.mqtt.protocol}://${this.mqtt.host}:${this.mqtt.port}${this.mqtt.path}`,
+        {
+          host: this.mqtt.host,
+          port: this.mqtt.port,
+          path: this.mqtt.path,
+          clientId: this.mqtt.clientId,
+          clean: this.mqtt.clean,
+          username: this.mqtt.username,
+          password: this.mqtt.password,
+          protocol: this.mqtt.protocol,
+
+          keepalive: 30,
+          reconnectPeriod: 1000,
+          connectTimeout: 30 * 1000,
+        });
+
+      this.mqtt.client.on('error', (err) => {
+        console.log(err);
+        this.mqtt.client.end();
+      });
+
+      this.mqtt.client.on('connect', () => {
+        console.log('client connected:' + this.mqtt.clientId);
+        this.mqtt.client.subscribe('#');
+      });
+
+      this.mqtt.client.on('message', (topic, message) => {
+        console.log('Received Message:= ' + message.toString() + '\nOn topic:= ' + topic);
+      });
+
+      this.mqtt.client.on('close', () => {
+        console.log(this.mqtt.clientId + ' disconnected');
+      });
+    },
+
+    resetAll() {
+      for (const input of this.inputs) {
+        this.setInput(input);
+      }
+    },
+
     setInput(input) {
-      axios.get(`/input?type=${input.type}&id=${input.id}&value=${input.value}`);
+      switch (input.type) {
+        case 'SPEED':
+          this.mqtt.client.publish(`setting/speed`, String(input.value));
+          break;
+        case 'SERVO':
+          this.mqtt.client.publish(`servo/${input.id}`, String(input.value));
+          break;
+      }
     }
   },
 };
 </script>
 
 <style lang="css">
-html, body {
-  margin: 0;
-  padding: 0;
-  font-family: "Helvetica Neue";
-}
+  html,
+  body {
+      margin: 0;
+      padding: 0;
+      font-family: 'Helvetica Neue';
+  }
 
-#app {
-  margin: 0;
-  padding: 3rem 1.5rem;
-}
+  #app {
+      margin: 0;
+      padding: 3rem 1.5rem;
+  }
 
-.input {
-  margin-top: 2em;
-  margin-bottom: 4em;
-}
+  .input {
+      margin-top: 2em;
+      margin-bottom: 4em;
+  }
 </style>
